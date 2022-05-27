53ebc2052d64499267e46b1bcde03e27f855f39c
Marco Lecheler <marco.lecheler@mercedes-benz.com>
2022-05-27 10:51:28 +0200

    feat: split version

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -16,7 +19,14 @@ type AppParameters struct {
 	bump    string
 }
 
+type Version struct {
+	major int
+	minor int
+	patch int
+}
+
 var parameters AppParameters
+var useVersionPrefix = false
 
 func main() {
 
@@ -32,4 +42,46 @@ func main() {
 		log.Fatal("no version specified")
 		os.Exit(1)
 	}
+
+	// if parameters.version starts with character v remove it
+	if parameters.version[0] == 'v' {
+		parameters.version = parameters.version[1:]
+		useVersionPrefix = true
+	}
+
+	v := splitVersion(parameters.version)
+
+	fmt.Println(v)
+}
+
+func splitVersion(version string) Version {
+	versionParts := strings.Split(version, ".")
+
+	if len(versionParts) != 3 {
+		log.Fatal("invalid version format - expected 3 integers (e.g. v1.2.3)")
+		os.Exit(1)
+	}
+
+	major, err := strconv.Atoi(versionParts[0])
+	if err != nil {
+		log.Fatal("could not parse major version - expected integer")
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	minor, err := strconv.Atoi(versionParts[1])
+	if err != nil {
+		log.Fatal("could not parse minor version - expected integer")
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	patch, err := strconv.Atoi(versionParts[2])
+	if err != nil {
+		log.Fatal("could not parse patch version - expected integer")
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	v := Version{major: major, minor: minor, patch: patch}
+
+	return v
 }
